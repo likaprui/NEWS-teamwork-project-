64f52009db3a3405c66c7d13cb4fc619b66a1438
likaprui <pruidzelika@gmail.com>
2024-10-19 00:31:21 +0400

    lika's commit

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-console.log("hello")
-console.log("bro")
+let img = document.getElementById("mainSliderImg")
+let i=0
+let pics = ["slider-img/ppl.png", "slider-img/office.png", "slider-img/magazine.jpg", "slider-img/social.png"]
+let circles = document.getElementsByClassName("balls")
+
+setInterval(() => {
+    for(let char of circles){
+        char.style.backgroundColor=""
+    }
+    
+    img.src=pics[i]
+    circles[i].style.backgroundColor="white"
+
+    if(i<pics.length){
+        i++
+
+    }if(i==pics.length){
+        i=0
+        
+    }
+
+}, 2000);

bf28627fde87c2dc2a7c1223aa2d712bd8ed2d78
MeliRoldan <meli.rww@gmail.com>
2024-10-31 19:57:14 +0400

    fix responsive

--- news.js
+++ news.js
@@ -21,6 +21,33 @@ menu.addEventListener("click", ()=> {
     navbar.classList.toggle('visible');
 })
 
+// contact page function
+let aContact=document.getElementById("a-contact")
+let mainPort=document.getElementsByTagName("main")[0]
+let contactPage=document.getElementsByClassName("contact-page")[0]
+aContact.addEventListener("click", ()=>{
+    contactPage.style.display="flex"
+    contactPage.style.width="90%"
+    document.body.style.backgroundColor="#F2EDE6"
+    mainPort.style.backgroundColor="#F2EDE6"
+    document.body.style.overflow="auto"
+    document.body.height="110vh"
+    document.querySelector("main").paddingBottom="70px"
+    mainPort.innerHTML=""
+    mainPort.style.display="flex"
+    mainPort.style.alignItems="center"
+    mainPort.style.borderRight="1px solid black"
+    mainPort.style.borderLeft="1px solid black"
+
+    mainPort.appendChild(contactPage)
+    console.log("jj")
+    document.body.style.height="auto"
+    document.querySelector("main").style.width="80%"
+    document.querySelector("header").style.borderBottom="1px solid black"
+
+
+})
+
 // news API
 
 let userLocation = "Georgia AND Tbilisi NOT state NOT Atlanta";
